68447c606fdc4126231de535611fbcdfbfc91ad0
shichenjia <592358796@qq.com>
2017-08-08 21:36:49 +0800

    update

--- server/User.js
+++ server/User.js
@@ -1,9 +1,5 @@
 const mongoose=require('mongoose');
-const db=mongoose.createConnection('mongodb://localhost:27017/videochat');
-db.once('open',function (err) {
-  console.log('数据库连接成功');
-});
-
+const db=require('./db');
 const userSchema=new mongoose.Schema({
   'username':String,
   'pwd':String
